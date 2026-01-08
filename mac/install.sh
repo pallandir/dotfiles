38a35cc339b5a2efc8a0efd5cac78d9e25bcec83
pallandir <pallandir2@gmail.com>
2026-01-08 22:22:54 +0800

    Add MacOS setup

---
+++ mac/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Istall and setup Homebrew
+
+## Install and config iterm2
+
+## Install and setup oh-my-zsh
+
+## Install and config Neovim
+
+## Install and setup tmux
+
+## Install and setup aerospace
