4b19be87d47aefa6d30f615064212276d3f58710
pallandir <pallandir2@gmail.com>
2026-01-09 16:59:20 +0800

    Add prerequesites and improve check_command_exec function

--- macos/install.sh
+++ macos/install.sh
@@ -1,23 +1,16 @@
 #!/bin/zsh
 
-## Preset
+## Global
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color (reset)
 
-function is_homebrew_installed() {
-    if command -v brew &> /dev/null; then
-        return 0
-    else
-        return 1
-    fi
-}
-
 function check_command_exec(){
+    local toolName="$1"
     if [ $? -eq 0 ];
     then
-        echo "${GREEN}Tool installed successfully${NC}"
+        echo "${GREEN} ${toolName} status: [OK] ${NC}"
     else
         echo "${RED}Tool installation failed aborting setup.${NC}"
         exit 0
@@ -40,14 +33,29 @@ function log_tool_install(){
     echo "${GREEN}Installing ${toolName}...${NC}"
 }
 
+function git_ssh_auth_ok() {
+  ssh -T git@github.com 2>&1 | grep -q "successfully authenticated"
+}
+
+## Prerequisites
+
+echo "\n${GREEN}Installing configuration for macOS${NC}\n"
+echo "${GREEN}Setup includes: \n - Homebrew \n - Iterm2 \n - ZSH (+ config) \n - Neovim (+ config) \n - Tmux \n - Aerospace tiling manager${NC}"
+if ! git_ssh_auth_ok; then
+    echo "\n${RED}You need a valid GitHub SSH key in order to clone configurations.\nFind more information here: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent.${NC}"
+else
+    echo "GitHub auth [OK]"
+fi
+exit 0
 
 ## Install and setup Homebrew
+
 
 TOOL="brew"
 log_tool_install "$TOOL"
 if ! is_tool_installed "$TOOL"; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    check_command_exec
+    check_command_exec "$TOOL"
 fi
 
 
@@ -57,7 +65,7 @@ TOOL="iterm2"
 log_tool_install "$TOOL"
 if is_tool_installed "brew"; then
     brew install --cask iterm2
-    check_command_exec
+    check_command_exec "$TOOL"
 fi
 
 ## Install and setup oh-my-zsh
@@ -68,7 +76,7 @@ if ! is_tool_installed "$TOOL"; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     echo "${GREEN}Installing Jovial theme for zsh...${NC}"
     curl -sSL https://github.com/zthxxx/jovial/raw/master/installer.sh | sudo -E bash -s ${USER:=`whoami`}
-    check_command_exec
+    check_command_exec "$TOOL"
 fi
 
 
@@ -78,9 +86,9 @@ TOOL="nvim"
 log_tool_install "$TOOL"
 if ! is_tool_installed "$TOOL"; then
     brew install neovim
-    check_command_exec
     echo "${GREEN}Cloning nvim config into ~/.config/nvim${NC}"
     git clone git@github.com:pallandir/neovim.git ~/.config/nvim
+    check_command_exec "$TOOL"
 fi
 
 ## Install and setup tmux
@@ -89,7 +97,7 @@ TOOL="tmux"
 log_tool_install "$TOOL"
 if ! is_tool_installed "$TOOL"; then
     brew install tmux
-    check_command_exec
+    check_command_exec "$TOOL"
 fi
 
 ## Install and setup aerospace
@@ -100,5 +108,5 @@ if ! is_tool_installed "$TOOL"; then
     brew install --cask aerospace
     mkdir -p ~/.config/aerospace
     cp aerospace.toml ~/.config/aerospace/
-    check_command_exec
+    check_command_exec "$TOOL"
 fi
